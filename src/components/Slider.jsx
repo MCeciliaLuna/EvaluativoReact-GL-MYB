35073b6982e91116f7cf62a25c10cd502fc942c7
Kibbax <cabello.marcos.ezequiel@gmail.com>
2023-08-03 21:22:23 -0300

    se crearon los archivos de estilos para Login page, slider, navbar y footer

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import styles from "./slider.module.css"; // Importamos los estilos desde el archivo slider.module.css
 
 const Slider = () => {
   const slides = [
@@ -14,15 +15,15 @@ const Slider = () => {
       title: "Título de la Imagen 2",
       description: "Descripción breve de la Imagen 2",
     },
-    // Añadir más objetos de imágenes y descripciones aquí...
+    // imágenes y descripciones aquí...
   ];
 
   return (
-    <Carousel autoPlay infiniteLoop showStatus={false} showThumbs={false}>
+    <Carousel autoPlay infiniteLoop showStatus={false} showThumbs={false} className={styles["carousel-root"]}>
       {slides.map((slide, index) => (
-        <div key={index}>
+        <div key={index} className={styles["carousel-slide"]}>
           <img src={slide.imageUrl} alt={`Imagen ${index + 1}`} />
-          <div className="legend">
+          <div className={styles.legend}>
             <h3>{slide.title}</h3>
             <p>{slide.description}</p>
           </div>
